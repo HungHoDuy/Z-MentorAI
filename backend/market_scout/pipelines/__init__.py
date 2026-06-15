3d3024a45971273691fc2e11bd57a22f31990b35
AnnaDo2812 <Doquynhnga235@gmail.com>
2026-06-15 14:22:51 +0700

    Normalize salary data

--- backend/market_scout/pipelines/__init__.py
+++
@@ -1,7 +0,0 @@
-from .clean_documents_pipeline import CleanDocumentsPipeline
-from .ingest_sources_pipeline import IngestSourcesPipeline
-
-__all__ = [
-    "CleanDocumentsPipeline",
-    "IngestSourcesPipeline",
-]
