a5044d2a6dcf16f42ba23333e04612a30a03b71d
HungHoDuy <hungduyhoqaz@gmail.com>
2026-08-31 21:37:05 +0700

    chore(infra): set container min instances to 0 in deployment workflow and terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -392,7 +392,7 @@ resource "google_cloud_run_v2_service" "academic_architect" {
 
   template {
     scaling {
-      min_instance_count = 1
+      min_instance_count = 0
     }
     containers {
       image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.name}/${var.academic_architect_image}"
